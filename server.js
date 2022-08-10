6f72c63315827f8231e3317c620e5503d5e12240
Ryangendel <gendel.ryan@gmail.com>
2022-08-09 18:37:33 -0600

    added static express

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require("path")
 
 const PORT = process.env.PORT || 3001
-
+app.use(express.static("build"))
 app.get("*", (req, res)=>{
     res.sendFile(path.join(__dirname,"build", "index.html"))
 })
